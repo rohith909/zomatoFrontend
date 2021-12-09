18aeca3e4b1ef028aea7d23f89fe4ddda5908f8c
Rohith <68690728+rohith909@users.noreply.github.com>
2021-12-09 10:30:21 +0530

    Update home.js

--- src/Components/home.js
+++ src/Components/home.js
@@ -17,7 +17,7 @@ class Home extends React.Component{
         sessionStorage.clear();
 
         axios({
-            url:'https://zomato-clone-9.herokuapp.com/location',
+            url:'https://videoappchats.herokuapp.com/location',
             method:'GET',
             headers:{'Content-Type' : 'application/json'}
         })
@@ -27,7 +27,7 @@ class Home extends React.Component{
         .catch( err => console.log(err))
 
         axios({
-            url:'https://zomato-clone-9.herokuapp.com/mealtype',
+            url:'https://videoappchats.herokuapp.com/mealtype',
             method:'GET',
             headers:{'Content-Type' : 'application/json'}
         })
